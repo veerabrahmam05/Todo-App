c6cf3a6d4a1c97d6fa7369f8f5f46adc604fd82b
veerabrahmam05 <veerabrahmamaluri@gmail.com>
2026-03-16 01:18:59 +0530

    feat(backend): added authentication for dummy data

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,17 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import ResponseValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
+from pydantic import BaseModel
+import jwt
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from datetime import timedelta, datetime, timezone
 
 from app.config.database import create_tables
 from app.routes import todo
@@ -17,6 +24,8 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 origins = os.getenv("CORS_ORIGINS", "").split(",")
 
@@ -39,3 +48,113 @@ async def response_validation_exception_handler(req: Request, e: ResponseValidat
             "errors": e.errors()
         }
     )
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
+        "disabled": False,
+    }
+}
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRES_IN_MINUTES = 30
+
+password_hash = PasswordHash.recommended()
+DUMMY_HASH_PASSWORD = password_hash.hash("dummypassword")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def hash_password(password: str):
+    return password_hash.hash(password)
+
+def get_user(db, username):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+    return None
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        verify_password(password, DUMMY_HASH_PASSWORD)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_error = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Could not validate Credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username = payload.get("sub")
+        if not username:
+            raise credentials_error
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_error
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_error
+    return user
+
+def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user"
+        )
+    return current_user
+    
+
+@app.post("/token", response_model=Token)
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires_in = timedelta(minutes=ACCESS_TOKEN_EXPIRES_IN_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires_in)
+
+    return Token(access_token=access_token, token_type="Bearer")
+
+@app.get("/user/me", response_model=User)
+async def user(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
